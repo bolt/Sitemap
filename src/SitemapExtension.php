59d3a5191b4572075bb0dd2fce643b848fce8529
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-22 13:30:18 +0100

    Update extension for Bolt v3

--- src/SitemapExtension.php
+++ src/SitemapExtension.php
@@ -1,71 +1,155 @@
 <?php
-// Sitemap Extension for Bolt, by Bob den Otter
 
 namespace Bolt\Extension\Bolt\Sitemap;
 
-use Bolt\Extensions\Snippets\Location as SnippetLocation;
+use Bolt\Asset\Snippet\Snippet;
+use Bolt\Asset\Target;
+use Bolt\Controller\Zone;
+use Bolt\Extension\SimpleExtension;
+use Bolt\Legacy\Content;
+use Carbon\Carbon;
+use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Response;
 
-# If we have a boatload of content, we might need a bit more memory.
-set_time_limit(0);
-ini_set('memory_limit', '512M');
-
-class SitemapExtension extends \Bolt\BaseExtension
+/**
+ * Sitemap extension for Bolt.
+ *
+ * @author Bob den Otter <bob@twokings.nl>
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class SitemapExtension extends SimpleExtension
 {
-    public function getName()
+    /**
+     * Route for regular sitemap.
+     *
+     * @return Response
+     */
+    public function sitemap($xml = false)
     {
-        return 'Sitemap';
+        $config = $this->getConfig();
+        $body = $this->renderTemplate($config['template'], ['entries' => $this->getLinks()]);
+
+        return new Response($body, Response::HTTP_OK);
     }
 
     /**
-     * Initialize Sitemap. Called during bootstrap phase.
+     * Route for XML based sitemap.
+     *
+     * @return Response
      */
-    public function initialize()
+    public function sitemapXml()
     {
-        if (empty($this->config['ignore_contenttype'])) {
-            $this->config['ignore_contenttype'] = array();
-        }
+        $config = $this->getConfig();
+        $body = $this->renderTemplate($config['xml_template'], ['entries' => $this->getLinks()]);
 
-        if (empty($this->config['ignore'])) {
-            $this->config['ignore'] = array();
-        }
+        $response = new Response($body, Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'application/xml; charset=utf-8');
 
-        if (empty($this->config['ignore_listing'])) {
-            $this->config['ignore_listing'] = false;
-        }
-
-        // Set up the routes for the sitemap..
-        $this->app->match('/sitemap', array($this, 'sitemap'));
-        $this->app->match('/sitemap.xml', array($this, 'sitemapXml'));
-
-        $this->addSnippet(SnippetLocation::END_OF_HEAD, 'headsnippet');
+        return $response;
     }
 
-    public function sitemap($xml = false)
+    /**
+     * Callback to generate the <link> inserted in the <head> section.
+     *
+     * @return string
+     */
+    public function snippetCallback()
     {
-        if ($xml) {
-            $this->app['extensions']->clearSnippetQueue();
-            $this->app['extensions']->disableJquery();
-            $this->app['debugbar'] = false;
-        }
+        $app = $this->getContainer();
+        $snippet = sprintf(
+            '<link rel="sitemap" type="application/xml" title="Sitemap" href="%ssitemap.xml">',
+            $app['resources']->getUrl('root')
+        );
 
-        $links = array(array('link' => $this->app['paths']['root'], 'title' => $this->app['config']->get('general/sitename')));
-        foreach ($this->app['config']->get('contenttypes') as $contenttype) {
-            if (!in_array($contenttype['slug'], $this->config['ignore_contenttype']) && !$contenttype['viewless'] &&
-                ((isset($contenttype['searchable']) && $contenttype['searchable']) ||  !isset($contenttype['searchable']))
-               ) {
+        return $snippet;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerAssets()
+    {
+        $snippet = new Snippet();
+        $snippet
+            ->setLocation(Target::END_OF_HEAD)
+            ->setZone(Zone::FRONTEND)
+            ->setCallback([$this, 'snippetCallback'])
+        ;
+
+        return [
+            $snippet,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Set up the routes for the sitemap.
+     */
+    protected function registerFrontendRoutes(ControllerCollection $collection)
+    {
+        $collection->match('sitemap', [$this, 'sitemap']);
+        $collection->match('sitemap.xml', [$this, 'sitemapXml']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefaultConfig()
+    {
+        return [
+            'ignore'             => [],
+            'ignore_contenttype' => [],
+            'ignore_listing'     => false,
+        ];
+    }
+
+    /**
+     * Get an array of links.
+     *
+     * @return array
+     */
+    private function getLinks()
+    {
+        // If we have a boatload of content, we might need a bit more memory.
+        set_time_limit(0);
+        ini_set('memory_limit', '512M');
+
+        $app = $this->getContainer();
+        $config = $this->getConfig();
+        $contentTypes = $app['config']->get('contenttypes');
+        $contentParams = ['limit' => 10000, 'order' => 'datepublish desc', 'hydrate' => false];
+        $rootPath = $app['resources']->getUrl('root');
+
+        $links = [
+            [
+                'link'  => $rootPath,
+                'title' => $app['config']->get('general/sitename'),
+            ],
+        ];
+        foreach ($contentTypes as $contentType) {
+            $searchable = (isset($contentType['searchable']) && $contentType['searchable']) || !isset($contentType['searchable']);
+            $isIgnored = in_array($contentType['slug'], $config['ignore_contenttype']);
+
+            if (!$isIgnored && !$contentType['viewless'] && $searchable) {
                 $baseDepth = 0;
-                if (isset($contenttype['listing_template']) && !$this->config['ignore_listing']) {
+                if (isset($contentType['listing_template']) && !$config['ignore_listing']) {
                     $baseDepth = 1;
-                    $links[] = array('link' => $this->app['paths']['root'] . $contenttype['slug'], 'title' => $contenttype['name'], 'depth' => 1);
+                    $links[] = [
+                        'link'  => $rootPath . $contentType['slug'],
+                        'title' => $contentType['name'],
+                        'depth' => 1,
+                    ];
                 }
-                $content = $this->app['storage']->getContent(
-                    $contenttype['slug'],
-                    array('limit' => 10000, 'order' => 'datepublish desc', 'hydrate' => false)
-                );
+                $content = $app['storage']->getContent($contentType['slug'], $contentParams);
+                /** @var Content $entry */
                 foreach ($content as $entry) {
-                    $links[] = array('link' => $entry->link(), 'title' => $entry->getTitle(), 'depth' => $baseDepth + 1,
-                        'lastmod'           => date(\DateTime::W3C, strtotime($entry->get('datechanged'))), 'record' => $entry, );
+                    $links[] = [
+                        'link'    => $entry->link(),
+                        'title'   => $entry->getTitle(),
+                        'depth'   => $baseDepth + 1,
+                        'lastmod' => Carbon::createFromTimestamp(strtotime($entry->get('datechanged')))->toW3cString(),
+                        'record'  => $entry,
+                    ];
                 }
             }
         }
@@ -76,60 +160,36 @@ class SitemapExtension extends \Bolt\BaseExtension
             }
         }
 
-        if ($xml) {
-            $template = $this->config['xml_template'];
-        } else {
-            $template = $this->config['template'];
-        }
-
-        $this->app['twig.loader.filesystem']->addPath(__DIR__);
-
-        $body = $this->app['render']->render($template, array(
-            'entries' => $links,
-        ));
-
-        $headers = array();
-        if ($xml) {
-            $headers['Content-Type'] = 'application/xml; charset=utf-8';
-        }
-
-        return new Response($body, 200, $headers);
+        return $links;
     }
 
-    public function linkIsIgnored($link)
+    /**
+     * Check to see if a link should be ignored from teh sitemap.
+     *
+     * @param string $link
+     *
+     * @return bool
+     */
+    private function linkIsIgnored($link)
     {
-        if (in_array($link['link'], $this->config['ignore'])) {
+        $config = $this->getConfig();
+
+        if (in_array($link['link'], $config['ignore'])) {
             // Perfect match
             return true;
         }
 
-        // use ignore as a regex
-        foreach ($this->config['ignore'] as $ignore) {
+        // Use ignore as a regex
+        foreach ($config['ignore'] as $ignore) {
             $pattern = str_replace('/', '\/', $ignore);
 
-            // Match on whole string so a $ignore of "/entry/" isn't the same as
-            // "/entry/.*"
+            // Match on whole string so a $ignore of "/entry/" isn't the same as "/entry/.*"
             if (preg_match("/^{$pattern}$/", $link['link'])) {
                 return true;
             }
         }
 
-        // no absolute match + no regex match
+        // No absolute match & no regex match
         return false;
-    }
-
-    public function sitemapXml()
-    {
-        return $this->sitemap(true);
-    }
-
-    public function headsnippet()
-    {
-        $snippet = sprintf(
-            '<link rel="sitemap" type="application/xml" title="Sitemap" href="%ssitemap.xml">',
-            $this->app['paths']['rooturl']
-        );
-
-        return $snippet;
     }
 }
